2cb6467b8a77ff0012ed632ef04eaa20bbacdf65
Corey96 <0coreyy@gmail.com>
2022-06-10 19:36:09 +0100

    created middleware

---
+++ server.js
@@ -0,0 +1,21 @@
+// importing express modules
+const express = require('express');
+const fs = require('fs')
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes.api');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// middleware for parsing JSON and urlencoded form data 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+ // start server
+ app.listen(PORT, () => {
+     console.log('Server live on port ${PORT}!`);
+ });
