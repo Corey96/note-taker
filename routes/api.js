4514b42be3fb4e183e92a5a2ef1b4ea89567ac28
Corey96 <0coreyy@gmail.com>
2022-06-11 15:34:13 +0100

    server bug fix

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,15 @@ const fs = require('fs');
 const uuid = require('uuid');
 
 // get function to return data
-router.get('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', 'utf-8, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return res.json(JSON.parse(data));
-        }
+router.get("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return res.json(JSON.parse(data));
+      }
     });
-});
+  });
 
 // post function to add new notes
 router.post("/api/notes", (req, res) => {
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs')
 const htmlRoutes = require('./routes/htmlRoutes')
-const apiRoutes = require('./routes.api');
+const apiRoutes = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 
